6ef9d15b8df3e0ed99bd27d70d3fd5c61eddaf81
Delmury <angel_2053@live.com>
2025-10-19 20:31:43 -0400

    1 al 5

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints a..z
+ */
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		_putchar(c);
+	_putchar('\n');
+}
---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _islower - checks lowercase
+ * @c: char code
+ * Return: 1 if lower, 0 otherwise
+ */
+int _islower(int c)
+{
+	return (c >= 'a' && c <= 'z');
+}
