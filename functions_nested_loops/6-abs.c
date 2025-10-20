915d7d5b3b67d37a52030e7397cbd99c0f2de9f4
Delmury <angel_2053@live.com>
2025-10-19 20:38:34 -0400

    6 al 8

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _abs - absolute value
+ * @n: integer
+ * Return: |n|
+ */
+int _abs(int n)
+{
+	return (n < 0 ? -n : n);
+}
---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints and returns last digit
+ * @n: integer
+ * Return: last digit
+ */
+int print_last_digit(int n)
+{
+	int d = n % 10;
+
+	if (d < 0)
+		d = -d;
+	_putchar('0' + d);
+	return (d);
+}
+
