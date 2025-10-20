9ed48eee1f2fb39751264325d8983acb31f11e9f
Delmury <angel_2053@live.com>
2025-10-19 20:25:54 -0400

    0-putchar.c

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - prints _putchar
+ * Return: 0
+ */
+int main(void)
+{
+	char s[] = "_putchar\n";
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+		_putchar(s[i]);
+	return (0);
+}
